583ae06e823e3965c0bc24cfae2c881f9d9becd2
webtangen <143550134+webtangen@users.noreply.github.com>
2023-12-09 10:37:56 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+def main():
+
+def generate_random_data():
+
+        print(f"Random Number: {item}")
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+import random
+    for item in data:
+
+
+    data = generate_random_data()
